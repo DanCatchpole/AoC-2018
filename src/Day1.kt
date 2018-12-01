5953daa7275e2b7fe39db5f8d9989476160c7b1c
DCatcher <dan@dcatcher.me>
2018-12-01 14:47:39 +0000

    One Liner for 1a

--- src/Day1.kt
+++ src/Day1.kt
@@ -4,17 +4,11 @@ import kotlin.system.measureTimeMillis
 class Day1 {
 
     companion object {
-        fun partA() : Int {
-            var score = 0
-            File("day1.txt").forEachLine {
-                score += it.toInt()
-            }
-            return score
-        }
+        fun partA() : Int = File("day1.txt").readLines().map { it.toInt() }.sum()
 
         fun partB() : Int {
             var currentFreq = 0
-            var prevFreqs: MutableSet<Int> = mutableSetOf()
+            val prevFreqs: MutableSet<Int> = mutableSetOf()
             val lines = File("day1.txt").readLines()
             while (true) {
                 for (line in lines) {
